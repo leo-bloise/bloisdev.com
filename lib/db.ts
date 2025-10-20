2334766f85e8f977c0e5dc97acb72afd27ee0620
Leonardo Bloise <leonardocsbloise@gmail.com>
2025-10-19 21:31:44 -0300

    fix: updating DATABASE_URL to NEON_DATABASE_URL

--- lib/db.ts
+++ lib/db.ts
@@ -1,9 +1,9 @@
 import { Client } from "@neondatabase/serverless";
 
 export function getClient() {
-    const url = process.env.DATABASE_URL;
+    const url = process.env.NEON_DATABASE_URL;
     if (!url) {
-        throw new Error("Missing DATABASE_URL in environment");
+        throw new Error("Missing NEON_DATABASE_URL in environment");
     }
     return new Client({ connectionString: url });
 }
